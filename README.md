608f4791a4f9db35e65d18ab11e0f6e608ae3130
moumita954 <73013219+moumita954@users.noreply.github.com>
2020-12-26 21:09:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BounceOff-algorithm-1
+# bounceOffAlgorithm
+Algorithm to bounce off two objects
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "pink";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 700,80,30);
+  movingRect.shapeColor = "blue";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = 5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);//as it will move to the opposite side
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  }
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+  drawSprites();
+}
